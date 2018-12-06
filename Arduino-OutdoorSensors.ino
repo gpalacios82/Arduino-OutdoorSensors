1a95b6f8d79415eea2621531b91be95b7e96210b
Gustavo Palacios <gpalacios82@gmail.com>
2018-12-06 20:14:17 +0100

    v1.2 (incluidos lux y voltajes)

--- Arduino-OutdoorSensors.ino
+++ Arduino-OutdoorSensors.ino
@@ -1,21 +1,26 @@
+// Incluimos librería para manejar el sensor de humedad y temperatura
 #include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11 // Tipos de sensor: DHT_TYPE_11 DHT_TYPE_21 DHT_TYPE_22
 
-#define DHT_SENSOR_TYPE DHT_TYPE_11 // DHT_TYPE_11 DHT_TYPE_21 DHT_TYPE_22
-
+// Definicion de puertos
 int DHT_SENSOR_PIN = 2;
+int LUX_SENSOR_PIN = A0;
+int VOLTAJE_SENSOR_PIN = A1;
+
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 
 void setup( )
 {
-  Serial.begin( 9600);
+  Serial.begin( 9600 );
+  pinMode(VOLTAJE_SENSOR_PIN, INPUT);
+  pinMode(LUX_SENSOR_PIN, INPUT);
 }
 
 
 static bool measure_environment( float *temperature, float *humidity )
 {
   static unsigned long measurement_timestamp = millis( );
-
   if( millis( ) - measurement_timestamp > 3000ul )
   {
     if( dht_sensor.measure( temperature, humidity ) == true )
@@ -24,7 +29,6 @@ static bool measure_environment( float *temperature, float *humidity )
       return( true );
     }
   }
-
   return( false );
 }
 
@@ -34,29 +38,18 @@ void loop( )
 {
   float temperature;
   float humidity;
-  String sensacion1;
-  String sensacion2;
 
+  // Leemos datos de luminosidad
+  float LuxQty = analogRead(LUX_SENSOR_PIN);
+  LuxQty = map(LuxQty, 0, 1023, 0, 100); 
+
+  // Leemos datos de Voltaje
+  float voltaje = 0.0048 * analogRead(VOLTAJE_SENSOR_PIN); 
+
+  // Si hay datos de humedad y temperatura (que es lo principal), procedemos a enviarlo por puerto serie
   if( measure_environment( &temperature, &humidity ) == true )
   {
-    if(temperature <= 15)
-      sensacion1 = String("Uff, parece que hace bastante frío, hay " + String(temperature) + " grados.");
-    else
-      if(temperature >= 25)
-        sensacion1 = String("Con " + String(temperature) + " grados, hoy parece que hace bastante calor.");
-      else
-        sensacion1 = String("Hay " + String(temperature) + " grados en la calle hoy.");
-
-    if(humidity <= 30)
-      sensacion2 = String("El ambiente es muy seco, hay una humedad del " + String(humidity) + " %.");   
-    else
-      if(humidity >= 70)
-        sensacion2 = String("El ambiente es súper húmedo, hay un " + String(humidity) + " %.");   
-      else
-        sensacion2 = String("La humedad es del " + String(humidity) + "%.");   
-
-    Serial.print( sensacion1 );
-    Serial.print(' ');
-    Serial.println( sensacion2 );
+    Serial.println( String("Temp:" + String(temperature) + "ºC;Humidity:" + String(humidity)) + "%;Luminosity:" + String(LuxQty) + "%;Voltaje:" + String(voltaje) + "V");
   }
+  //delay(500);
 }
